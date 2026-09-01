4277cd6a4532e471f4bf7286ed4fd718dff7ffd6
AmanthaSanuja <amanthasanuja9@gmail.com>
2026-09-01 13:05:03 +0530

    Update p5.java

--- p5/p5.java
+++ p5/p5.java
@@ -1,14 +1,31 @@
 import java.util.Random;
 class p5{
     
-    
-
-    public static void main(String[]args){
-       Random rand = new Random(99);
+    public static void cal(){
+        Random rand = new Random();
         String[] operators = {"+", "-"};
-        String expression = "";
-        
 
+        int sum=0;
+
+        while(sum != 100){
+            int num1 = rand.nextInt(99) + 1;
+            int num2 = rand.nextInt(99) + 1;
+            String operator = operators[rand.nextInt(operators.length)];
+            if(operator.equals("+")){
+                sum = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + sum);
+            } else {
+                sum = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + sum);
+            }
+
+        }
+
+        
     }
 
+    public static void main(String[]args){
+        cal();
+
+    }
 }
